cc324a8eedfe1963b13e3447d4fd10884e41b4a0
Joe Lukose <joelukose@gmail.com>
2017-10-15 09:00:37 -0400

    Initial Commit

---
+++ src/main/java/com/example/demo/LearnAngularApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAngularApplication.class, args);
+	}
+}
